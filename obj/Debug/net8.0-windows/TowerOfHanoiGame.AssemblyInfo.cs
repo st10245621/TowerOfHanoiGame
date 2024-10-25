b1ef196400b39c63ceddf639a0925331c9d675ff
Gerard <104638929+st10245621@users.noreply.github.com>
2024-10-25 16:12:49 +0200

    updated background

--- obj/Debug/net8.0-windows/TowerOfHanoiGame.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/TowerOfHanoiGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TowerOfHanoiGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35244ef812c5534e5c6acd67d7447834689c85a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d50248adb067a2fa0d5a1d3c3c900211f7aeca2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TowerOfHanoiGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TowerOfHanoiGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
